2d8a1d56c4327b5f7938055ff2a7f05e73b05d81
codx <201314288+monocodx@users.noreply.github.com>
2025-04-03 04:43:15 +0800

    build: update build scripts to include module dependency handling for automated builds

--- build.bat
+++ build.bat
@@ -6,6 +6,9 @@ rmdir "bin" /s /q
 @rem remove build cache
 go clean -cache
 
+@rem module depndencies
+go mod tidy -C src
+
 @rem executable binary
 go build -C src  -o ..\bin\localfs.exe  -ldflags="-s -w" -x
 
--- build.sh
+++ build.sh
@@ -6,5 +6,8 @@ rm -rf bin
 # remove build cache
 go clean -cache
 
+# module depndencies
+go mod tidy -C src
+
 # build executable binary
 go build -C src  -o ../bin/localfs  -ldflags="-s -w" -x
